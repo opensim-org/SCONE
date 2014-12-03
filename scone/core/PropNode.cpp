0008c5a291711e270163dbb63096b80fc14d9f9c
Thomas Geijtenbeek <Thomas.Geijtenbeek@MotekMedical.com>
2014-12-03 17:15:51 +0100

    Added initial version of PropNode

---
+++ scone/core/PropNode.cpp
@@ -0,0 +1,260 @@
+#include "stdafx.h"
+
+#include "PropNode.h"
+
+#include <set>
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/property_tree/info_parser.hpp>
+#include <boost/tokenizer.hpp>
+#include <boost/algorithm/string.hpp>
+#include "boost/foreach.hpp"
+
+using namespace boost;
+using namespace boost::property_tree;
+
+namespace scone
+{
+	PropNode::PropNode()
+	{
+	}
+
+	PropNode::PropNode( const char* prop_string )
+	{
+		if ( prop_string == 0 || strlen(prop_string) == 0 )
+			return;
+
+		std::string props( prop_string );
+		tokenizer< char_separator<char> > tok(props, char_separator<char>(";") );
+		for ( tokenizer< char_separator<char> >::iterator iter = tok.begin(); iter != tok.end(); ++iter )
+		{
+			size_t pos = iter->find_first_of('=');
+			if (pos != std::string::npos)
+				AddChild( trim_copy( iter->substr(0, pos) ) )->m_Value = StringValue( trim_copy( iter->substr(pos + 1) ) );
+		}
+	}
+
+	PropNode::PropNode( const PropNode& other )
+	{
+		*this = other;
+	}
+
+	PropNode& PropNode::operator=( const PropNode& other )
+	{
+		m_Value = other.m_Value;
+		m_Children.clear();
+		for ( ConstChildIter iter = other.m_Children.begin(); iter != other.m_Children.end(); ++iter )
+			m_Children.push_back( std::make_pair( iter->first, PropNodePtr( new PropNode( *iter->second ) ) ) );
+		return *this;
+	}
+
+	PropNode& PropNode::Merge( const PropNode& other, bool overwrite )
+	{
+		if ( overwrite )
+			m_Value = other.m_Value;
+
+		for ( ConstChildIter other_it = other.m_Children.begin(); other_it != other.m_Children.end(); ++other_it )
+		{
+			// see if we already have the key
+			PropNodePtr child = nullptr;
+			for ( ChildContainer::iterator this_it = m_Children.begin(); this_it != m_Children.end(); ++this_it )
+			{
+				if ( this_it->first == other_it->first )
+					child = this_it->second;
+			}
+
+			if ( child == nullptr )
+				m_Children.push_back( std::make_pair( other_it->first, PropNodePtr( new PropNode( *other_it->second ) ) ) );
+			else child->Merge( *other_it->second, overwrite );
+		}
+
+		return *this;
+	}
+
+	PropNode::~PropNode()
+	{
+	}
+
+	PropNodePtr PropNode::AddChild( const String& key )
+	{
+		size_t ofs = key.find_first_of( '.' );
+		if ( ofs == String::npos )
+		{
+			// create first-level child
+			m_Children.push_back( std::make_pair( key, PropNodePtr( new PropNode ) ) );
+			return m_Children.back().second;
+		}
+		else
+		{
+			// create sub-children if necessary
+			String head_key = key.substr( 0, ofs );
+			String tail_key = key.substr( ofs + 1 );
+			PropNodePtr head_child = GetChildPtr( head_key );
+			if ( head_child == nullptr )
+				head_child = AddChild( head_key );
+
+			return head_child->AddChild( tail_key );
+		}
+	}
+
+	const PropNodePtr PropNode::GetChildPtr( const String& key ) const
+	{
+		size_t ofs = key.find_first_of( '.' );
+		if ( ofs == String::npos )
+		{
+			// find first-level child
+			for ( ConstChildIter iter = m_Children.begin(); iter != m_Children.end(); ++iter )
+			{
+				if ( iter->first == key )
+					return iter->second;
+			}
+			return nullptr;
+		}
+		else
+		{
+			// search children
+			String head_key = key.substr( 0, ofs );
+			String tail_key = key.substr( ofs + 1 );
+
+			for ( ConstChildIter iter = m_Children.begin(); iter != m_Children.end(); ++iter )
+			{
+				if ( iter->first == head_key )
+				{
+					PropNodePtr p = iter->second->GetChildPtr( tail_key );
+					if ( p != nullptr )
+						return p; // child has been found
+				}
+			}
+			return nullptr;
+		}
+	}
+
+	void PropNode::RemoveChild( const String& key )
+	{
+		size_t ofs = key.find_first_of( '.' );
+		if ( ofs == String::npos )
+		{
+			// remove first matching child
+			for ( ChildIter child_iter = m_Children.begin(); child_iter != m_Children.end(); ++child_iter )
+			{
+				if ( child_iter->first == key )
+				{
+					m_Children.erase( child_iter );
+					return;
+				}
+			}
+		}
+		else
+		{
+			// search sub keys
+			String head_key = key.substr( 0, ofs );
+			String tail_key = key.substr( ofs + 1 );
+			PropNodePtr head_child = GetChildPtr( head_key );
+			head_child->RemoveChild( tail_key );
+			if ( head_child->IsEmpty() )
+				RemoveChild( head_key );
+		}
+	}
+
+	void PropNode::RemoveChildren( const String& key )
+	{
+		size_t ofs = key.find_first_of( '.' );
+		if ( ofs == String::npos )
+		{
+			// remove all children
+			ChildIter child_iter = m_Children.begin();
+			while ( child_iter != m_Children.end() )
+			{
+				if ( child_iter->first == key )
+					child_iter = m_Children.erase( child_iter );
+				else ++child_iter;
+			}
+		}
+		else
+		{
+			// search sub keys
+			String head_key = key.substr( 0, ofs );
+			String tail_key = key.substr( ofs + 1 );
+			PropNodePtr head_child = GetChildPtr( head_key );
+			head_child->RemoveChildren( tail_key );
+			if ( head_child->IsEmpty() )
+				RemoveChild( head_key );
+		}
+	}
+
+	void FromPropertyTree( PropNode& props, const ptree& tree )
+	{
+		props.Set( tree.get_value("") );
+		BOOST_FOREACH( const ptree::value_type &v, tree )
+		{
+			PropNodePtr child = props.AddChild( v.first );
+			FromPropertyTree( *child, v.second );
+		}
+	}
+
+	void ToPropertyTree( ptree& tree, const PropNode& props, const String& key )
+	{
+		tree.put( key, props.GetValue().GetData() );
+		String sub_key = key.empty() ? "" : key + ".";
+		for ( PropNode::ConstChildIter iter = props.Begin(); iter != props.End(); ++iter )
+			ToPropertyTree( tree, *iter->second, sub_key + iter->first );
+	}
+
+	void PropNode::ToXmlFile( const String& filename )
+	{
+		ptree pt;
+		ToPropertyTree( pt, *this, m_Value );
+		boost::property_tree::xml_writer_settings<char> settings('\t', 1);
+		write_xml( filename, pt, std::locale(), settings );
+	}
+
+	void PropNode::ToIniFile( const String& filename )
+	{
+		ptree pt;
+		ToPropertyTree( pt, *this, m_Value );
+		write_ini( filename, pt );
+	}
+
+	void PropNode::ToInfoFile( const String& filename )
+	{
+		ptree pt;
+		ToPropertyTree( pt, *this, m_Value );
+		write_info( filename, pt );
+	}
+
+	void PropNode::FromXmlFile( const String& filename )
+	{
+		ptree pt;
+		read_xml( filename, pt, boost::property_tree::xml_parser::trim_whitespace );
+		FromPropertyTree( *this, pt );
+	}
+
+	void PropNode::FromIniFile( const String& filename )
+	{
+		ptree pt;
+		read_ini( filename, pt );
+		FromPropertyTree( *this, pt );
+	}
+
+	void PropNode::FromInfoFile( const String& filename )
+	{
+		ptree pt;
+		read_info( filename, pt );
+		FromPropertyTree( *this, pt );
+	}
+
+	void PropNode::ToStream( std::ostream& str, const std::string& prefix ) const
+	{
+		for ( ConstChildIter iter = m_Children.begin(); iter != m_Children.end(); ++iter )
+		{
+			str << prefix << iter->first;
+			if ( iter->second->HasValue() )
+				str << " = " << iter->second->GetValue().GetData();
+			str << std::endl;
+
+			iter->second->ToStream( str, prefix + "  " );
+		}
+	}
+}
---
+++ scone/core/PropNode.h
@@ -0,0 +1,253 @@
+#pragma once
+
+#include "core.h"
+
+#include <memory>
+#include <vector>
+#include <sstream>
+#include <type_traits>
+
+#include "Vec3.h"
+#include "Exception.h"
+
+namespace scone
+{
+	class PropNode;
+	typedef std::shared_ptr< PropNode > PropNodePtr;
+
+	class StringValue
+	{
+	public:
+		StringValue() { };
+		StringValue( const char* value ) : m_Data( value ) { };
+		StringValue( const StringValue& other ) : m_Data( other.m_Data ) { };
+		StringValue& operator=( const StringValue& other ) { m_Data = other.m_Data; return (*this); }
+		template< typename T > StringValue( const T& value ) { *this = value; }
+		~StringValue() { };
+
+		// get value of any type T
+		template< typename T >
+		operator T() const
+		{
+			std::istringstream str( m_Data );
+			T value;
+			str >> value;
+			return value;
+		}
+
+		// set values to any type T
+		template< typename T >
+		StringValue& operator=( const T& value )
+		{
+			std::ostringstream str;
+			str << value;
+			m_Data = str.str();
+			return (*this);
+		}
+
+		const String& GetData() const { return m_Data; }
+		bool IsEmpty() const { return m_Data.empty(); }
+
+	private:
+		String m_Data;
+	};
+
+	class CORE_API PropNode
+	{
+	public:
+
+		typedef std::pair< String, PropNodePtr > KeyChildPair;
+		typedef std::vector< KeyChildPair > ChildContainer;
+		typedef ChildContainer::iterator ChildIter;
+		typedef ChildContainer::const_iterator ConstChildIter;
+
+		PropNode();
+		PropNode( const PropNode& other );
+		PropNode( const char* prop_string );
+		~PropNode();
+
+		// assignment
+		PropNode& operator=( const PropNode& other );
+
+		// check if empty
+		bool IsEmpty()
+		{
+			return m_Children.empty() && m_Value.IsEmpty();
+		}
+
+		// check if empty
+		void Clear()
+		{
+			m_Children.clear();
+			m_Value = "";
+		}
+
+		// check if child key exists
+		bool HasKey( const String& key ) const
+		{
+			return GetChildPtr( key ) != nullptr;
+		}
+
+		// get value
+		const StringValue& GetValue() const
+		{
+			return m_Value;
+		}
+
+		// get value
+		bool HasValue() const
+		{
+			return !m_Value.IsEmpty();
+		}
+
+		// get and cast value
+		template< typename T >
+		T Get() const
+		{
+			return static_cast< T >( m_Value );
+		}
+
+		// set value
+		template< typename T >
+		void Set( const T& value )
+		{
+			m_Value = value;
+		}
+
+		/// Get value, throws exception if key doesn't exist
+		template< typename T >
+		T Get( const String& key ) const
+		{
+			PropNodePtr p = GetChildPtr( key );
+			if ( p == nullptr )
+				SCONE_THROW( "Could not find key: " + key );
+			else return p->Get<T>();
+		}
+
+		/// Get value, returns default if key doesn't exist
+		template< typename T >
+		T Get( const String& key, const T& default_value ) const
+		{
+			PropNodePtr p = GetChildPtr( key );
+			return p ? p->Get<T>() : default_value;
+		}
+
+		/// Copy child properties, empty if not existing
+		PropNode CopyChild( const String& key ) const
+		{
+			PropNodePtr p = GetChildPtr( key );
+			if ( p == nullptr )
+				return PropNode();
+			else return PropNode( *p );
+		}
+
+		/// Set a value, overwriting existing (if any) or adding new key
+		template< typename T >
+		PropNode& Set( const String& key, const T& value )
+		{
+			PropNodePtr p = GetChildPtr( key );
+			if ( p == nullptr )
+				p = AddChild( key );
+			p->Set( value );
+			return *this;
+		}
+
+		/// Set a value, always adding new key
+		template< typename T >
+		PropNode& Add( const String& key, const T& value )
+		{
+			AddChild( key )->Set( value );
+			return *this;
+		}
+
+		/// Get Child
+		const PropNodePtr GetChildPtr( const String& key ) const;
+
+		/// create child node
+		PropNodePtr AddChild( const String& key );
+
+		/// Merge existing properties
+		PropNode& Merge( const PropNode& props, bool overwrite = true );
+
+		/// Access to children for iteration
+		const ChildContainer& GetChildren() const { return m_Children; }
+		ConstChildIter Begin() const { return m_Children.cbegin(); }
+		ConstChildIter End() const { return m_Children.cend(); }
+
+		/// File I/O
+		void FromXmlFile( const String& filename );
+		void FromIniFile( const String& filename );
+		void FromInfoFile( const String& filename );
+
+		void ToXmlFile( const String& filename );
+		void ToIniFile( const String& filename );
+		void ToInfoFile( const String& filename );
+
+		/// Shortcut 'Get' functions for lazy people
+		int GetInt( const String& key ) const { return Get< int >( key ); }
+		bool GetBool( const String& key ) const { return Get< bool >( key ); }
+		Real GetReal( const String& key ) const { return Get< Real >( key ); }
+		String GetStr( const String& key ) const { return Get< String >( key ); }
+		Vec3 GetVec3( const String& key ) const { return Get< Vec3 >( key ); }
+
+		int GetInt( const String& key, int def ) const { return Get< int >( key, def ); }
+		bool GetBool( const String& key, bool def ) const { return Get< bool >( key, def ); }
+		Real GetReal( const String& key, Real def ) const { return Get< Real >( key, def ); }
+		String GetStr( const String& key, const String& def ) const { return Get< String >( key, def ); }
+		Vec3 GetVec3( const String& key, const Vec3& def ) const { return Get< Vec3 >( key, def ); }
+
+		friend std::ostream& operator<<( std::ostream& str, const PropNode& props ) { props.ToStream( str ); return str; }
+
+	private:
+
+		void ToStream( std::ostream& str, const std::string& prefix = "" ) const;
+		void RemoveChildren( const String& key );
+		void RemoveChild( const String& key );
+
+		StringValue m_Value;
+		ChildContainer m_Children;
+	};
+
+	// top level write
+	template< typename T >
+	void WriteData( T& v, PropNode& props ) {
+		v.ProcessData( props, false );
+	}
+
+	// top level read
+	template< typename T >
+	void ReadData( T& v, PropNode& props ) {
+		v.ProcessData( props, true );
+	}
+
+	// class types process
+	template< typename T >
+	void ProcessData( PropNode& props, bool read, const char* name, T& v, typename std::enable_if<std::is_class<T>::value >::type* = 0 )
+	{
+		if ( read )
+		{
+			PropNodePtr p = props.GetChildPtr( name );
+			v.ProcessData( *p, true );
+		}
+		else
+		{
+			PropNodePtr p = props.AddChild( name );
+			v.ProcessData( *p, false );
+		}
+	}
+
+	// fundamental types process
+	template< typename T >
+	void ProcessData( PropNode& props, bool read, const char* name, T& v, typename std::enable_if<std::is_fundamental<T>::value >::type* = 0 )
+	{
+		if ( read )
+		{
+			v = props.Get<T>( name );
+		}
+		else
+		{
+			props.Add( name, v );
+		}
+	}
+};
+
