469c8f57926f3d5453dbf20b71da1199e6c20406
Thomas Geijtenbeek <Thomas.Geijtenbeek@MotekMedical.com>
2015-01-13 16:16:29 +0100

    Added new project: sconeopt

---
+++ scone/sconeopt/sconeopt.cpp
@@ -0,0 +1,54 @@
+#include "stdafx.h"
+
+#include <conio.h>
+
+#include <boost/filesystem.hpp>
+#include <boost/algorithm/string.hpp>
+
+#include "../core/Log.h"
+#include "../opt/Optimizer.h"
+#include "../cs/SimulationObjective.h"
+
+using namespace boost::filesystem;
+using namespace scone;
+
+PropNode LoadProperties( const path& config_file )
+{
+	SCONE_LOG( "Reading file " << config_file );
+
+	if ( config_file.extension() == ".xml" )
+		return LoadXmlFile( config_file.string() );
+	else SCONE_THROW( "Unknown file type: " + config_file.string() );
+}
+
+int main(int argc, char* argv[])
+{
+	try
+	{
+		opt::RegisterFactoryTypes();
+		cs::RegisterFactoryTypes();
+
+		// get config file
+		path config_file( argc > 1 ? String( argv[ 1 ] ) : "config/optimization_test.xml" );
+
+		// load properties
+		PropNode p = LoadProperties( config_file );
+
+		// create optimizer and report unused parameters
+		opt::OptimizerUP o = CreateFromPropNode< opt::Optimizer >( p.GetChild( "Optimizer" ) );
+		p.ToStream( std::cout, "Unused parameter ", true );
+
+		// copy config and model file
+		path outdir( o->GetOutputFolder() );
+		copy_file( config_file, outdir / ( "config" + config_file.extension().string() ), copy_option::overwrite_if_exists );
+
+		o->Run();
+	}
+	catch (std::exception& e)
+	{
+		SCONE_LOG( "Exception: " << e.what() );		
+		_getch();
+	}
+
+	return 0;
+}
---
+++ scone/sconeopt/stdafx.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <SDKDDKVer.h>
+
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
+#include <windows.h>
+
+#include <stdio.h>
+#include <tchar.h>
+#include <string>
+#include <vector>
