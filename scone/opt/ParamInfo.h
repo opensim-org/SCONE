7cc73bc3a73b8bcf4b7ad79f2a0658be6e180a2a
Thomas Geijtenbeek <Thomas.Geijtenbeek@MotekMedical.com>
2015-02-14 00:11:56 +0100

    Some code comments in ParamInfo

--- scone/opt/ParamInfo.h
+++ scone/opt/ParamInfo.h
@@ -9,17 +9,25 @@ namespace scone
 	namespace opt
 	{
 		// TODO: rename to Param and store value as well. Why not?
+		// One possible reason: we only need these for optimization, but not for the end result
 		struct OPT_API ParamInfo
 		{
 			ParamInfo( const String& i_name, double i_init_mean, double i_init_std, double i_init_min, double i_init_max, double i_min, double i_max );
 			ParamInfo( const String& i_name, const PropNode& props );
 
-			/// public member variables
+			/// param id
 			String name;
+
+			/// current state
+			double value; // TODO: use this
 			double mean;
 			double std;
+
+			/// initialization options
 			double init_min;
 			double init_max;
+
+			/// parameter properties
 			double min;
 			double max;
 			bool is_free;
