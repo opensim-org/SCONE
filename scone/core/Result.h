e3c2858e10101903575b8e05328f38fb61daefff
Thomas Geijtenbeek <Thomas.Geijtenbeek@MotekMedical.com>
2015-08-18 16:57:38 +0200

    Minor updates to Result class

--- scone/core/Result.h
+++ scone/core/Result.h
@@ -2,32 +2,32 @@
 
 namespace scone
 {
-	template< CodeT = int >
+	template< typename T >
 	struct Result
 	{
 		virtual operator bool() const = 0;
 		virtual const char* message() const = 0;
-		virtual CodeT code() const = 0;
+		virtual T code() const { return code_; }
+	private:
+		T code_;
 	};
 
-	template< CodeT = int >
-	struct Success : Result< CodeT >
+	template< typename T >
+	struct Success : Result< T >
 	{
+		Success( const T& code ) : code_( code ) {}
 		virtual operator bool() const override { return true; }
 		virtual const char* message() const override { return ""; }
-		virtual CodeT code() const override { return CodeT( 0 ); }
 	};
 
-	template< CodeT = int >
+	template< typename T >
 	struct Failure : Result< CodeT >
 	{
-		Failure( CodeT code, String message = "" ) : code_( code ), msg_( message ) { }
+		Failure( const T& code, String message = "" ) : code_( code ), msg_( message ) { }
 		virtual operator bool() const { return false; }
 		virtual const char* message() const override { return msg_.c_str(); }
 		virtual CodeT code() const override { return code_; }
-
 	private:
-		CodeT code_;
 		String msg_;
 	};
 }
