4d54f8eb37c5277d5110419d97c3cd1e3ffabfb2
ongcf <none@none>
2015-10-08 14:39:19 -0700

    update and cleanup library linking in cmakelists. fix type mismatches that occur in 64bit due to differences in unsigned and size_t
    --HG--
    branch : cmake-64bit

--- scone/cs/FeedForwardController.cpp
+++ scone/cs/FeedForwardController.cpp
@@ -24,7 +24,7 @@ namespace scone
 		Controller( props, par, model, target_area )
 		{
 			DECLARE_AND_INIT( props, bool, use_symmetric_actuators, true );
-			INIT_PROPERTY( props, number_of_modes, 0u );
+			INIT_PROPERTY( props, number_of_modes, size_t( 0 ) );
 
 			// setup actuator info
 			for ( size_t idx = 0; idx < model.GetMuscles().size(); ++idx )
--- scone/cs/Polynomial.cpp
+++ scone/cs/Polynomial.cpp
@@ -13,7 +13,7 @@ namespace scone
 	Polynomial::Polynomial( const PropNode& props, opt::ParamSet& par )
 	{
 		size_t degree;
-		INIT_PROPERTY( props, degree, 0u );
+		INIT_PROPERTY( props, degree, size_t( 0 ) );
 		m_Coeffs.resize( degree + 1 );
 		for ( size_t i = 0; i < m_Coeffs.size(); ++i )
 			SetCoefficient( i, par.Get( GetStringF( "C%d", i ), props, GetStringF( "coefficient%d", i ) ) );
