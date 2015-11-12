a6937f9989deb23668b6d951a08c095a4bc9d9a0
ongcf <ongcf@stanford.edu>
2015-11-12 15:53:07 -0800

    add unix support for LARGE_INTEGER. could move elsewhere?
    --HG--
    branch : build-linux

--- scone/core/stdafx.h
+++ scone/core/stdafx.h
@@ -19,6 +19,25 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+// enable LARGE_INTEGER
+#include <stdint.h>
+typedef uint8_t BYTE;
+typedef uint32_t DWORD;
+typedef int32_t LONG;
+typedef int64_t LONGLONG;
+
+typedef union _LARGE_INTEGER {
+    struct {
+        DWORD LowPart;
+        LONG HighPart;
+    };
+    struct {
+        DWORD LowPart;
+        LONG HighPart;
+    } u;
+    LONGLONG QuadPart;
+} LARGE_INTEGER, *PLARGE_INTEGER;
+
 #endif
 
 #include <string>
