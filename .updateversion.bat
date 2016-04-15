4e40f464260398146c675928c55393beaa68da0f
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2016-04-15 16:15:41 +0200

    updated .updateversion.bat to work with git instead of hg

--- .updateversion.bat
+++ .updateversion.bat
@@ -1,3 +1,1 @@
-hg log -r . --template "{rev}" > "%~dp0\.version"
-hg log -r . --template "{rev}" > "%~dp0\bin\v100-Release\.version"
-hg log -r . --template "{rev}" > "%~dp0\bin\v120-Release\.version"
+git show -s --format=%%ct > "%~dp0\.version"
