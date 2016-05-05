62b88c54bd57d9866711d9562a81b3e926fe3812
carmichaelong <carmichael.ong@gmail.com>
2016-05-05 15:12:26 -0700

    fix propnode_test to work with new models folder structure

--- scone/sconeunittests/propnode_test.cpp
+++ scone/sconeunittests/propnode_test.cpp
@@ -6,7 +6,7 @@ using namespace boost::filesystem;
 BOOST_AUTO_TEST_CASE( propnode_stream_test )
 {
 	path rootpath = scone::GetSconeFolder( "root" );
-	path p1path = rootpath / "config/gait_GHW.xml";
+	path p1path = rootpath / "config/f1022_gait_GH.xml";
 	auto p1 = scone::ReadPropNodeFromXml( p1path.string() );
 
 	auto p2path = temp_directory_path() / p1path.filename();
