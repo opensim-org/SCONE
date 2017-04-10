1bf138b09538a3ed189c55279de72b0eb583e87a
Thomas Geijtenbeek <tgeijten@gmail.com>
2017-04-10 17:00:20 +0200

    initial version of ExternalBalanceController (work in progress)

---
+++ scone/sconelib/scone/controllers/ExternalBalanceController.cpp
@@ -0,0 +1,37 @@
+#include "ExternalBalanceController.h"
+
+#include "scone/model/Model.h"
+#include "scone/optimization/ParamSet.h"
+
+namespace scone
+{
+
+	ExternalBalanceController::ExternalBalanceController( const PropNode& props, ParamSet& par, Model& model, const Area& target_area ) :
+	Controller( props, par, model, target_area ),
+	force_body( *FindByName( model.GetBodies(), props.get< String >( "body" ) ) )
+	{
+		INIT_PROP_REQUIRED( props, name );
+		INIT_PARAM( props, par, angular_velocity_gain, 0.0 );
+		INIT_PARAM( props, par, angular_velocity_target, 0.0 );
+		INIT_PARAM( props, par, angular_position_gain, 0.0 );
+		INIT_PARAM( props, par, angular_position_target, 0.0 );
+	}
+
+	void ExternalBalanceController::StoreData( Storage<Real>::Frame& frame )
+	{
+		double pitch_pos = flut::math::pitch( force_body.GetOrientation() ).rad_value();
+		double pitch_vel = force_body.GetAngVel().z;
+		frame[ name + ".pp" ] = pitch_pos;
+		frame[ name + ".pv" ] = pitch_vel;
+	}
+
+	scone::Controller::UpdateResult ExternalBalanceController::UpdateControls( Model& model, double timestamp )
+	{
+		return Controller::SuccessfulUpdate;
+	}
+
+	String ExternalBalanceController::GetClassSignature() const
+	{
+		return "E";
+	}
+}
---
+++ scone/sconelib/scone/controllers/ExternalBalanceController.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "scone/core/types.h"
+#include "scone/model/Controller.h"
+#include "scone/core/PropNode.h"
+#include "scone/optimization/ParamSet.h"
+#include "scone/core/Vec3.h"
+
+namespace scone
+{
+	class SCONE_API ExternalBalanceController : public Controller
+	{
+	public:
+		ExternalBalanceController( const PropNode& props, ParamSet& par, Model& model, const Area& target_area );
+
+		String name;
+		double angular_velocity_gain;
+		double angular_velocity_target;
+		double angular_position_gain;
+		double angular_position_target;
+
+		virtual UpdateResult UpdateControls( Model& model, double timestamp ) override;
+		virtual void StoreData( Storage<Real>::Frame& frame ) override;
+
+	protected:
+		virtual String GetClassSignature() const override;
+
+	private:
+		Index state_idx;
+		Body& force_body;
+	};
+}
