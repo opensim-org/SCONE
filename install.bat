c77942cde43a793e47c2c21ba67952bfb6cbbc3c
Thomas Geijtenbeek <tgeijten@gmail.com>
2017-08-22 12:09:17 +0200

    removed Pod DV from N4

--- install.bat
+++ install.bat
@@ -4,4 +4,4 @@ xcopy "%~dp0\bin\msvc2015_64\Release\*" "%target_folder%\bin\msvc2015_64\Release
 xcopy "%~dp0\scenarios\*" "%target_folder%\scenarios\*" /S /Y
 xcopy "%~dp0\models\*" "%target_folder%\models\*" /S /Y
 xcopy "%~dp0\resources\*" "%target_folder%\resources\*" /S /Y
-xcopy "%~dp0.version" "%target_folder%" /S /Y
+xcopy "%~dp0\*" "%target_folder%\*" /S /Y
