f9361acbf26f79e2ecc64c6a3605ce32c1a7494e
Thomas Geijtenbeek <tgeijten@gmail.com>
2017-08-25 14:42:56 +0200

    SensorNeuron now has rectified output, added "inverted" flag

--- scone/sconelib/scone/controllers/Neuron.cpp
+++ scone/sconelib/scone/controllers/Neuron.cpp
@@ -16,6 +16,58 @@
 
 namespace scone
 {
+	SensorNeuron::SensorNeuron( const PropNode& pn, Params& par, Model& model, Locality loc ) :
+	Neuron( "" ),
+	input_(),
+	offset_(),
+	sensor_gain_( 1.0 )
+	{
+		INIT_PROP( pn, delay_, 999 );
+
+		const auto type = pn.get< string >( "type" );
+		bool opposite = pn.get_any< bool >( { "mirrored", "opposite" }, false );
+		bool inverted = pn.get< bool >( "inverted", false );
+		INIT_PROP( pn, offset_, type == "L" ? -1 : ( inverted ? 1 : 0 ) );
+		INIT_PROP( pn, sensor_gain_, inverted ? -1 : 1 );
+
+		const auto source_name = pn.get< string >( "source", "leg" );
+		par_name_ = source_name + ( opposite ? "_o." : "." ) + type;
+
+		if ( opposite )
+			loc = MakeMirrored( loc );
+
+		if ( type == "F" )
+		{
+			const auto sided_source_name = loc.ConvertName( source_name );
+			input_ = &model.AcquireDelayedSensor< MuscleForceSensor >( *FindByName( model.GetMuscles(), sided_source_name ) );
+		}
+		else if ( type == "L" )
+		{
+			const auto sided_source_name = loc.ConvertName( source_name );
+			input_ = &model.AcquireDelayedSensor< MuscleLengthSensor >( *FindByName( model.GetMuscles(), sided_source_name ) );
+		}
+		else if ( type == "DP" )
+		{
+			input_ = &model.AcquireDelayedSensor< DofPositionSensor >( *FindByName( model.GetDofs(), source_name ) );
+		}
+		else if ( type == "DV" )
+		{
+			input_ = &model.AcquireDelayedSensor< DofVelocitySensor >( *FindByName( model.GetDofs(), source_name ) );
+		}
+		else if ( type == "LD" )
+		{
+			input_ = &model.AcquireDelayedSensor< LegLoadSensor >( model.GetLeg( loc ) );
+		}
+
+		flut_assert_msg( input_, "Unknown type " + type );
+		name_ = input_->GetName() + stringf( "+%.0f", delay_ * 1000 );
+	}
+
+	double SensorNeuron::GetOutput() const
+	{
+		return output_ = ActivationFunction( sensor_gain_ * input_->GetValue( delay_ ) + offset_ );
+	}
+
 	InterNeuron::InterNeuron( const PropNode& pn, Params& par, Model& model, NeuralController& controller, const Locality& loc ) :
 	Neuron( loc.ConvertName( pn.get< string >( "name" ) ) )
 	{
@@ -47,53 +99,7 @@ namespace scone
 		activation_t value = offset_;
 		for ( auto& i : inputs_ )
 			value += i.first * i.second->GetOutput();
-		return output_ = std::max( 0.0, value );
-	}
-
-	SensorNeuron::SensorNeuron( const PropNode& pn, Params& par, Model& model, Locality loc ) :
-	Neuron( "" ),
-	input_()
-	{
-		INIT_PROP( pn, delay_, 999 );
-		const auto type = pn.get< string >( "type" );
-		const auto source_name = pn.get< string >( "source", "leg" );
-		bool opposite = pn.get_any< bool >( { "mirrored", "opposite" }, false );
-		par_name_ = source_name + ( opposite ? "_o." : "." ) + type;
-
-		if ( opposite )
-			loc = MakeMirrored( loc );
-
-		if ( type == "F" )
-		{
-			const auto sided_source_name = loc.ConvertName( source_name );
-			input_ = &model.AcquireDelayedSensor< MuscleForceSensor >( *FindByName( model.GetMuscles(), sided_source_name ) );
-		}
-		else if ( type == "L" )
-		{
-			const auto sided_source_name = loc.ConvertName( source_name );
-			input_ = &model.AcquireDelayedSensor< MuscleLengthSensor >( *FindByName( model.GetMuscles(), sided_source_name ) );
-			offset_ = -1;
-		}
-		else if ( type == "DP" )
-		{
-			input_ = &model.AcquireDelayedSensor< DofPositionSensor >( *FindByName( model.GetDofs(), source_name ) );
-		}
-		else if ( type == "DV" )
-		{
-			input_ = &model.AcquireDelayedSensor< DofVelocitySensor >( *FindByName( model.GetDofs(), source_name ) );
-		}
-		else if ( type == "LD" )
-		{
-			input_ = &model.AcquireDelayedSensor< LegLoadSensor >( model.GetLeg( loc ) );
-		}
-
-		flut_assert_msg( input_, "Unknown type " + type );
-		name_ = input_->GetName() + stringf( "+%.0f", delay_ * 1000 );
-	}
-
-	double SensorNeuron::GetOutput() const
-	{
-		return output_ = input_->GetValue( delay_ ) + offset_;
+		return output_ = ActivationFunction( value );
 	}
 
 	void MotorNeuron::UpdateActuator()
