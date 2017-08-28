09439f16876301271d018cbe969ff35210a05c2f
Thomas Geijtenbeek <tgeijten@gmail.com>
2017-08-28 17:28:37 +0200

    submodule updates, install fix

--- install.bat
+++ install.bat
@@ -4,4 +4,4 @@ xcopy "%~dp0bin\msvc2015_64\Release\*" "%target_folder%\bin\msvc2015_64\Release\
 xcopy "%~dp0scenarios\*" "%target_folder%\scenarios\*" /S /Y
 xcopy "%~dp0models\*" "%target_folder%\models\*" /S /Y
 xcopy "%~dp0resources\*" "%target_folder%\resources\*" /S /Y
-xcopy "%~dp0.version" "%target_folder%" /S /Y
+xcopy "%~dp0.version" "%target_folder%" /Y
