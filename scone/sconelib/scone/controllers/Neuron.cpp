ad75b99e7c3a9c9801b97c79f221264aa4bd55e7
Thomas Geijtenbeek <tgeijten@gmail.com>
2017-09-11 12:38:30 +0200

    added #if instead of comment

--- scone/sconelib/scone/controllers/Neuron.cpp
+++ scone/sconelib/scone/controllers/Neuron.cpp
@@ -6,8 +6,11 @@ namespace scone
 {
 	double Neuron::ActivationFunction( double input ) const
 	{
-		//return std::max( 0.0, input );
+#if 0
+		return std::max( 0.0, input );
+#else
 		const double scale = 0.02 / std::log( 2 );
 		return scale * std::log( 1 + std::exp( input / scale ) );
+#endif
 	}
 }
