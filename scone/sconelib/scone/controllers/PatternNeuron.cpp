c9c6c985eeda5356dcf9d6181f2aef6cea6bacbd
Thomas Geijtenbeek <tgeijten@gmail.com>
2017-09-19 17:00:07 +0200

    PatternNeuron par naming change

--- scone/sconelib/scone/controllers/PatternNeuron.cpp
+++ scone/sconelib/scone/controllers/PatternNeuron.cpp
@@ -17,12 +17,20 @@ namespace scone
 		auto amount = pn.get< int >( "amount" );
 
 		name_ = stringf( "CPG%d", index );
-		ScopedParamSetPrefixer pf( par, name_ + '.' );
-		t0_ = par.get( "t0", ( index + 1 ) * period_ / ( amount + 1 ), period_ / ( amount + 1 ) );
+		ScopedParamSetPrefixer pf( par, "CPG." );
+
+		auto tname = "t" + to_str( index );
+		if ( pn.has_key( tname ) )
+			t0_ = par.get( tname, pn[ tname ] );
+		else t0_ = par.get( tname, ( index + 1 ) * period_ / ( amount + 1 ), period_ / ( amount + 1 ) );
 		if ( mirrored )
 			t0_ = fmod( t0_ + period_ / 2, period_ );
 
-		INIT_PAR( pn, par, width_, period_ / ( amount + 1 ) );
+		auto wname = "w" + to_str( index );
+		if ( pn.has_key( wname ) )
+			width_ = par.get( wname, pn[ wname ] );
+		else width_ = par.try_get( wname, pn, "width", period_ / ( amount + 1 ) );
+
 		auto c = width_ / 2.35482;
 		beta_ = 1 / ( 2 * c * c );
 	}
