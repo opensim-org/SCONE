52e50ec442a4e223f5025386e19aa8b7931d8c4b
Thomas Geijtenbeek <tgeijten@gmail.com>
2017-10-27 16:35:38 +0200

    added Event class for step detection

---
+++ scone/sconelib/scone/core/Event.cpp
@@ -0,0 +1,47 @@
+#include "Event.h"
+
+namespace scone
+{
+	std::vector< Event > DetectGrfEvents( const Storage< Real, TimeInSeconds>& s, double threshold, double timespan )
+	{
+		std::vector< Event > e;
+
+		// find GRF channels
+		Index right_idx = s.GetChannelIndex( "leg1_r.grf_y" );
+
+		// detect events
+		for ( Index frame_idx = 1; frame_idx < s.GetFrameCount(); ++frame_idx )
+		{
+			TimeInSeconds time = s.GetFrame( frame_idx ).GetTime();
+			Real grf = s.GetFrame( frame_idx )[ right_idx ];
+			Real prev_grf = s.GetFrame( frame_idx - 1 )[ right_idx ];
+
+			if ( grf > 0 && prev_grf == 0 )
+				e.push_back( Event{ time, Event::RightHeelStrike } );
+			if ( grf == 0 && prev_grf > 0 )
+				e.push_back( Event{ time, Event::RightToeOff } );
+		}
+
+		// filter events
+		if ( e.size() > 2 )
+		{
+			for ( Index i = 0; i < e.size() - 2; ++i )
+			{
+				if ( e[ i ].event == Event::RightHeelStrike )
+				{
+					// detect 'bump': short contact followed by long flight
+					if ( e[ i + 1 ].time - e[ i ].time < timespan && e[ i + 2 ].time - e[ i + 1 ].time > timespan )
+						e.erase( e.begin() + i, e.begin() + 2 );
+				}
+				else if ( e[ i ].event == Event::RightToeOff )
+				{
+					// detect short flight
+					if ( e[ i + 1 ].time - e[ i ].time < timespan )
+						e.erase( e.begin() + i, e.begin() + 2 );
+				}
+			}
+		}
+
+		return e;
+	}
+}
