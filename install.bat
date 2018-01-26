b4938613a1d9e688f3c06696729a5943f54aaf55
Thomas Geijtenbeek <tgeijten@gmail.com>
2018-01-26 17:53:33 +0100

    install now copies msvc2017 binaries too

--- install.bat
+++ install.bat
@@ -1,6 +1,7 @@
 set target_folder=%Dropbox%\Development\scone
 
 xcopy "%~dp0bin\msvc2015_64\Release\*" "%target_folder%\bin\msvc2015_64\Release\*" /S /D /Y
+xcopy "%~dp0bin\msvc2017_64\Release\*" "%target_folder%\bin\msvc2017_64\Release\*" /S /D /Y
 xcopy "%~dp0tutorials\*" "%target_folder%\scenarios\tutorials\*" /S /Y
 xcopy "%~dp0resources\*" "%target_folder%\resources\*" /S /Y
 xcopy "%~dp0.version" "%target_folder%" /Y
