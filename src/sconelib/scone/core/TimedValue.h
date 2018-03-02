b7d916f054fabf4e8e75dd61aa7bfc65e50c9c1b
Thomas Geijtenbeek <tgeijten@gmail.com>
2018-03-02 17:55:32 +0100

    cleanup

--- src/sconelib/scone/core/TimedValue.h
+++ src/sconelib/scone/core/TimedValue.h
@@ -7,17 +7,8 @@ namespace scone
 	{
 	public:
 		TimedValue( const T& value = T(0), TimeInSeconds time = 0.0 ) : m_Value( value ), m_Time( time ) { };
-
 		operator const T&() const { return m_Value; }
-
-		void Set( const T& value, TimeInSeconds time ) {
-			if ( value != m_Value )
-			{
-				m_Value = value;
-				m_Time = time;
-			}
-		}
-
+		void Set( const T& value, TimeInSeconds time ) { if ( value != m_Value ) { m_Value = value; m_Time = time; } }
 		TimeInSeconds GetTime() { return m_Time; }
 			
 	private:
