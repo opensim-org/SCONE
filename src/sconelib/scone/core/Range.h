2dabcc4a27ac942935e0c046cae03b0c7e2773f8
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2018-06-14 12:25:48 +0200

    xo compatibility fixes

--- src/sconelib/scone/core/Range.h
+++ src/sconelib/scone/core/Range.h
@@ -10,7 +10,7 @@ namespace scone
 	class Range
 	{
 	public:
-		Range( const PropNode& props ) : min( props.get< T >( "min", xo::lowest<T>() ) ), max( props.get< T >( "max", xo::max<T>() ) ) {}
+		Range( const PropNode& props ) : min( props.get< T >( "min", xo::const_lowest<T>() ) ), max( props.get< T >( "max", xo::const_max<T>() ) ) {}
 		Range( const T& i_min, const T& i_max ) : min( i_min ), max( i_max ) {}
 		Range() : min( T() ), max( T() ) {}
 
