ecad9ad1f3716ce7f546daaafc6b6f823d218933
Thomas Geijtenbeek <tgeijten@gmail.com>
2018-06-19 13:48:41 +0200

    visualization updates

--- src/sconestudio/SconeStudio.cpp
+++ src/sconestudio/SconeStudio.cpp
@@ -91,9 +91,9 @@ scene( true )
 
 	// init scene
 	scene.add_light( vis::vec3f( -20, 80, 40 ), vis::make_white( 1 ) );
-	ground_plane = scene.add< vis::plane >( 64, 64, 1, scone::GetStudioSetting< vis::color >( "viewer.tile1" ), scone::GetStudioSetting< vis::color >( "viewer.tile2" ) );
+	//ground_plane = scene.add< vis::plane >( 64, 64, 1, scone::GetStudioSetting< vis::color >( "viewer.tile1" ), scone::GetStudioSetting< vis::color >( "viewer.tile2" ) );
+	ground_plane = scene.add< vis::plane >( xo::vec3f( 64, 0, 0 ), xo::vec3f( 0, 0, -64 ), GetFolder( SCONE_UI_RESOURCE_FOLDER ) / "stile1.png" );
 
-	//scene.create_tile_floor( 64, 64, 1, scone::GetStudioSetting< vis::color >( "viewer.tile1" ), scone::GetStudioSetting< vis::color >( "viewer.tile2" ) );
 	ui.osgViewer->setClearColor( make_osg( scone::GetStudioSetting< vis::color >( "viewer.background" ) ) );
 }
 
