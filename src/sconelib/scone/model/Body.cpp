c51d8c0fd75d1b5ca534a680fc954028b3cd0b9b
Thomas Geijtenbeek <tgeijten@gmail.com>
2018-08-07 10:55:15 +0200

    U: Body now outputs com_pos, lin_vel, ori, ang_vel

--- src/sconelib/scone/model/Body.cpp
+++ src/sconelib/scone/model/Body.cpp
@@ -20,12 +20,16 @@ namespace scone
 	void Body::StoreData( Storage<Real>::Frame& frame, const StoreDataFlags& flags ) const
 	{
 		auto& name = GetName();
-		if ( flags( StoreDataTypes::BodyOriginPosition ) )
+		if ( flags( StoreDataTypes::BodyComPosition ) )
 		{
-			auto pos = GetOriginPos();
-			frame[ name + ".pos_x" ] = pos.x;
-			frame[ name + ".pos_y" ] = pos.y;
-			frame[ name + ".pos_z" ] = pos.z;
+			auto pos = GetComPos();
+			frame[ name + ".com_x" ] = pos.x;
+			frame[ name + ".com_y" ] = pos.y;
+			frame[ name + ".com_z" ] = pos.z;
+			auto lin_vel = GetComVel();
+			frame[ name + ".lin_vel_x" ] = lin_vel.x;
+			frame[ name + ".lin_vel_y" ] = lin_vel.y;
+			frame[ name + ".lin_vel_z" ] = lin_vel.z;
 		}
 		if ( flags( StoreDataTypes::BodyOrientation ) )
 		{
@@ -33,6 +37,10 @@ namespace scone
 			frame[ name + ".ori_x" ] = ori.x;
 			frame[ name + ".ori_y" ] = ori.y;
 			frame[ name + ".ori_z" ] = ori.z;
+			auto ang_vel = GetAngVel();
+			frame[ name + ".ang_vel_x" ] = ang_vel.x;
+			frame[ name + ".ang_vel_y" ] = ang_vel.y;
+			frame[ name + ".ang_vel_z" ] = ang_vel.z;
 		}
 	}
 }
