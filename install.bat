44c64ec7c2bafc3d9e8e2471a8d5b4dc9ba6ccb8
Thomas Geijtenbeek <tgeijten@gmail.com>
2018-09-12 15:35:06 +0200

    R: removed tutorials, added Examples

--- install.bat
+++ install.bat
@@ -2,6 +2,6 @@ set target_folder=%Dropbox%\Development\scone
 call %~dp0.updateversion.bat
 xcopy "%~dp0bin\msvc2015_64\Release\*" "%target_folder%\bin\msvc2015_64\Release\*" /S /D /Y
 xcopy "%~dp0bin\msvc2017_64\Release\*" "%target_folder%\bin\msvc2017_64\Release\*" /S /D /Y
-xcopy "%~dp0tutorials\*" "%target_folder%\scenarios\tutorials\*" /S /D /Y
+xcopy "%~dp0Examples\*" "%target_folder%\scenarios\Examples\*" /S /D /Y
 xcopy "%~dp0resources\*" "%target_folder%\resources\*" /S /D /Y
 xcopy "%~dp0.version" "%target_folder%" /D /Y
