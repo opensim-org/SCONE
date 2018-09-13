aa51d8317ad1c791bbdd2305a26f57d3317d28a7
Thomas Geijtenbeek <tgeijten@gmail.com>
2018-09-13 17:13:18 +0200

    U: doxygen

--- src/sconelib/scone/optimization/TestObjective.h
+++ src/sconelib/scone/optimization/TestObjective.h
@@ -4,16 +4,19 @@
 
 namespace scone
 {
+	/// Objective used for testing, evaluates a multi-dimensional Schwefel function.
 	class SCONE_API TestObjective : public Objective
 	{
 	public:
 		TestObjective( const PropNode& pn );
 		virtual ~TestObjective() {}
-		
+
+		/// Dimension of the objective function
+		size_t dim_;
+
 		virtual fitness_t evaluate( const SearchPoint& point ) const override;
 
 	protected:
-		size_t dim_;
 		virtual String GetClassSignature() const override;
 
 	};
