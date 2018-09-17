e190edc2d19d0a99ee076ea154275d59848d647b
Thomas Geijtenbeek <tgeijten@gmail.com>
2018-09-17 15:20:02 +0200

    D: Optimizer doc

--- src/sconelib/scone/optimization/Optimizer.h
+++ src/sconelib/scone/optimization/Optimizer.h
@@ -18,11 +18,48 @@ namespace scone
 		Optimizer( const PropNode& props );
 		virtual ~Optimizer();
 
+		/// Parameter file (.par) to be used for initial parameter values.
+		path init_file;
+
+		/// Use init_file (if exists); default = true.
+		bool use_init_file;
+
+		/// Use the standard deviations from the init_file; when set to false, the initial standard deviation is 
+		/// computed as follows: STDEV = parameter value * init_std_factor + init_std_offset; default = true.
+		bool use_init_file_std;
+
+		/// Factor by which to multiply the standard deviations from the init_file; default = 1.0.
+		double init_file_std_factor;
+
+		/// Offset added to the standard deviations from init_file; default = 0.
+		double init_file_std_offset;
+
+		/// Maximum number of threads to use for this optimization; default = 32.
+		size_t max_threads;
+
+		/// Thread priority of the optimization; 0 = lowest, 7 = highest, default = 1.
+		int thread_priority;
+
+		/// Number of iterations after which to stop the optimization; default = 10000.
+		size_t max_generations;
+
+		/// Minimum progress after which to stop the optimization; default = 1e-6.
+		double min_progress;
+
+		/// Minimum number of samples after which progress is measured; default = 200.
+		size_t min_progress_samples;
+
+		/// The minimum improvement factor needed for file output, default = 1.05.
+		Real min_improvement_factor_for_file_output;
+
+		/// The maximum number of iterations wihtout file output, default = 1000.
+		size_t max_generations_without_file_output;
+
 		Objective& GetObjective() { return *m_Objective; }
 		const Objective& GetObjective() const { return *m_Objective; }
 		virtual void Run() = 0;
 
-		/// get the results output folder (creates it if it doesn't exist)
+		// get the results output folder (creates it if it doesn't exist)
 		const path& GetOutputFolder() const;
 
 		bool IsBetterThan( double v1, double v2 ) { return IsMinimizing() ? v1 < v2 : v1 > v2; }
@@ -50,30 +87,10 @@ namespace scone
 
 		const String& id() const { return id_; }
 
-		path output_root;
-
-		// properties
-		size_t max_threads;
-		int thread_priority;
-		bool show_optimization_time;
-
-		// init file
-		path init_file;
-		bool use_init_file;
-		bool use_init_file_std;
-		double init_file_std_factor;
-		double init_file_std_offset;
-
-		// file output
-		Real min_improvement_factor_for_file_output;
-		size_t max_generations_without_file_output;
-		bool output_objective_result_files;
 		mutable size_t m_LastFileOutputGen;
-
-		// stop conditions
-		size_t max_generations;
-		double min_progress;
-		size_t min_progress_samples;
+		path output_root;
+		bool show_optimization_time;
+		bool output_objective_result_files;
 
 		void ManageFileOutput( double fitness, const std::vector< path >& files ) const;
 
