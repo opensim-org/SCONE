be20d1ab9f48eb2831ff5d1dfb160d4c3029ac9a
Thomas Geijtenbeek <tgeijten@gmail.com>
2018-11-16 16:25:40 +0100

    U: version bump

--- src/sconelib/scone/core/version.cpp
+++ src/sconelib/scone/core/version.cpp
@@ -13,7 +13,7 @@
 
 constexpr int SCONE_VERSION_MAJOR = 0;
 constexpr int SCONE_VERSION_MINOR = 19;
-constexpr int SCONE_VERSION_PATCH = 0;
+constexpr int SCONE_VERSION_PATCH = 1;
 constexpr char* SCONE_VERSION_POSTFIX = SCONE_DEBUG ? "ALPHA DEBUG" : "ALPHA";
 
 namespace scone
