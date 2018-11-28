a93a9e61bc28d32a3f43d8dbc9fb508cb7186ccd
Thomas Geijtenbeek <tgeijten@gmail.com>
2018-11-28 14:10:13 +0100

    F: sconeunittest exception handling

--- src/sconeunittests/main.cpp
+++ src/sconeunittests/main.cpp
@@ -3,8 +3,18 @@
 
 int main( int argc, const char* argv[] )
 {
-	optimization_test();
-	propnode_stream_test();
+	xo::log::console_sink sink( xo::log::info_level );
+
+	try
+	{
+		optimization_test();
+		propnode_stream_test();
+	}
+	catch ( std::exception& e)
+	{
+		log::critical( "Could not perform tests: ", e.what() );
+		return -1;
+	}
 
 	return XO_TEST_REPORT();
 }
