e42c20a870b9f7dfc1b6894aa82f0166f2de7b75
Thomas Geijtenbeek <tgeijten@gmail.com>
2019-02-18 17:00:48 +0100

    R: moved doxygen config, removed install.bat

--- install.bat
+++
@@ -1,6 +0,0 @@
-set target_folder=%Dropbox%\Development\scone
-call %~dp0.updateversion.bat
-xcopy "%~dp0bin\msvc2015_64\Release\*" "%target_folder%\bin\msvc2015_64\Release\*" /S /D /Y
-xcopy "%~dp0bin\msvc2017_64\Release\*" "%target_folder%\bin\msvc2017_64\Release\*" /S /D /Y
-xcopy "%~dp0resources\*" "%target_folder%\resources\*" /S /D /Y
-xcopy "%~dp0.version" "%target_folder%" /D /Y
