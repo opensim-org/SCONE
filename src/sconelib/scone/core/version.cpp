6d08c09aa9d4058bbaae6313de98bcfe7954cb33
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2019-11-11 12:51:06 +0100

    U: version postfix

--- src/sconelib/scone/core/version.cpp
+++ src/sconelib/scone/core/version.cpp
@@ -14,7 +14,7 @@
 constexpr int SCONE_VERSION_MAJOR = 1;
 constexpr int SCONE_VERSION_MINOR = 3;
 constexpr int SCONE_VERSION_PATCH = 0;
-constexpr const char* SCONE_VERSION_POSTFIX = "PREVIEW RELEASE 1";
+constexpr const char* SCONE_VERSION_POSTFIX = "";
 
 namespace scone
 {
