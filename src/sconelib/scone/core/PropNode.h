53aecd26366c3384dd7a42f33558f0e6275998ed
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2019-11-15 11:10:23 +0100

    R: PropNode.h no longer includes xo/serialization/serialize.h

--- src/sconelib/scone/core/PropNode.h
+++ src/sconelib/scone/core/PropNode.h
@@ -12,12 +12,8 @@
 
 #include "xo/container/prop_node.h"
 #include "xo/container/prop_node_tools.h"
-#include "xo/serialization/serialize.h"
 
 namespace scone
 {
 	using PropNode = xo::prop_node;
-	using xo::load_file;
-	using xo::load_file_with_include;
-	using xo::save_file;
 }
