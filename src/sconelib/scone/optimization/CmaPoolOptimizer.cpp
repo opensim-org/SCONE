6851ddf559a91ef0b8df1ec8fa86496970685a77
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2020-01-02 16:44:03 +0100

    U: changed CmaPoolOptimizer default optimizations to 6

--- src/sconelib/scone/optimization/CmaPoolOptimizer.cpp
+++ src/sconelib/scone/optimization/CmaPoolOptimizer.cpp
@@ -20,7 +20,7 @@ namespace scone
 		INIT_PROP( pn, prediction_window_, 300 );
 		INIT_PROP( pn, prediction_start_, 100 );
 		INIT_PROP( pn, prediction_look_ahead_, 1000 );
-		INIT_PROP( pn, optimizations_, 12 );
+		INIT_PROP( pn, optimizations_, 6 );
 		INIT_PROP( pn, active_optimizations_, 6 );
 		INIT_PROP( pn, concurrent_optimizations_, 2 );
 		INIT_PROP( pn, random_seed_, 1 );
