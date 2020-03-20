b9f9d3e821778204c8212614a7f3c64ac9b57460
Dimitar Stanev <jimstanev@gmail.com>
2020-03-20 21:39:13 +0100

    StepMeasure restricts stride length instead of step length

--- src/sconelib/scone/measures/StepMeasure.cpp
+++ src/sconelib/scone/measures/StepMeasure.cpp
@@ -12,24 +12,17 @@
 #include "scone/core/Exception.h"
 #include "scone/core/profiler_config.h"
 #include "scone/core/GaitCycle.h"
-// #include "xo/container/sorted_vector.h"
-// #include "xo/container/container_algorithms.h"
 
 namespace scone
 {
-	StepMeasure::StepMeasure( const PropNode& props, Params& par, const Model& model, const Location& loc ) :
+	StepMeasure::StepMeasure( const PropNode& props, Params& par,
+							  const Model& model, const Location& loc ) :
 	Measure( props, par, model, loc )
 	{
-		INIT_PROP( props, step_length, RangePenalty<Real>() );
-		INIT_PROP( props, load_threshold, 0.2 );
+		INIT_PROP_REQUIRED( props, stride_length );
+		INIT_PROP( props, load_threshold, 0.01 );
 		INIT_PROP( props, min_step_duration, 0.2 );
-		INIT_PROP( props, initiation_steps, 2 );
-
-		// check if step_length range penalty is defined by the user
-		SCONE_THROW_IF(initiation_steps < 1, "The initiation_steps must be greater than zero.");
-
-		// check if step_length range penalty is defined by the user
-		SCONE_THROW_IF(step_length.IsNull(), "The step_length RangePenalty is not defined.");
+		INIT_PROP( props, initiation_cycles, 1 );
 	}
 
 	bool StepMeasure::UpdateMeasure( const Model& model, double timestamp )
@@ -56,50 +49,24 @@ namespace scone
 
 	double StepMeasure::ComputeResult( const Model& model )
 	{
-		auto cycles = ExtractGaitCycles( stored_data_, load_threshold, min_step_duration );
+		auto cycles = ExtractGaitCycles( stored_data_,
+										 load_threshold, min_step_duration );
+
+		// calculate step length
 		double penalty = 0;
-		std::cout << "cycles: " << cycles.size() << std::endl;
-		if (cycles.size() + 1 > initiation_steps)
+		for (int cycle = initiation_cycles; cycle < cycles.size(); cycle++) {
+			auto t = cycles[cycle].begin_;
+			auto stride_len = cycles[cycle].length();
+			stride_length.AddSample( t, stride_len );
+			// log::TraceF("%d %.3f @ %.3f", cycle, t, stride_len );
+		}
+
+		// calculate penalty
+		if ( !stride_length.IsNull() && stride_length.GetNumSamples() > 0 )
 		{
-			// calculate step length
-			int skip = initiation_steps;
-			int cycle = 1;
-			for (; cycle < cycles.size(); cycle++) {
-				skip--;
-				if (skip >= 0) continue;
-
-				auto t = cycles[cycle - 1].begin_;
-				auto step_len = xo::length(cycles[cycle].begin_pos_ - cycles[cycle - 1].begin_pos_);
-				log::TraceF("%d %.3f @ %.3f", cycle, t, step_len );
-				std::cout << t << std::endl;
-				step_length.AddSample( t, step_len );
-			}
-			std::cout << "final one " << std::endl;
-			// count the two steps of the final cycle
-			auto c1 = cycles[cycle - 2];
-			auto c2 = cycles[cycle - 1];
-			if (skip < 0) // initiation_steps might be an odd number equal to the total steps - 1
-			{
-				auto t1 =  c2.begin_;
-				std::cout << t1 << std::endl;
-				auto step_len1 = xo::length(c1.end_pos_ - c2.begin_pos_);
-				log::TraceF("f1 %.3f @ %.3f", t1, step_len1 );
-				step_length.AddSample( t1, step_len1 );
-			}
-			std::cout << "final two " << std::endl;
-			auto t2 = c1.end_;
-			std::cout << t2 << std::endl;
-			auto step_len2 = xo::length(c2.end_pos_ - c1.end_pos_);
-			log::TraceF("f2 %.3f @ %.3f", t2, step_len2 );
-			step_length.AddSample( t2, step_len2 );
-			std::cout << "end " << std::endl;
-
-			// calculate penalty
-			if ( !step_length.IsNull() && step_length.GetNumSamples() > 0 )
-			{
-				penalty += step_length.GetResult();
-				GetReport().set( "step_length_penalty" , stringf( "%g", step_length.GetResult() ) );
-			}
+			penalty += stride_length.GetResult();
+			GetReport().set( "stride_length_penalty" ,
+							 stringf( "%g", stride_length.GetResult() ) );
 		}
 
 		return penalty;
@@ -107,12 +74,6 @@ namespace scone
 
 	String StepMeasure::GetClassSignature() const
 	{
-		return stringf( "L" );
-	}
-
-	void StepMeasure::StoreData( Storage< Real >::Frame& frame, const StoreDataFlags& flags ) const
-	{
-		if ( !step_length.IsNull() && step_length.GetNumSamples() > 0)
-			frame[ GetName() + ".step_length_penalty" ] = step_length.GetLatest();
+		return stringf( "SL" );
 	}
 }
--- src/sconelib/scone/measures/StepMeasure.h
+++ src/sconelib/scone/measures/StepMeasure.h
@@ -12,11 +12,11 @@
 
 namespace scone
 {
-	/// Measure for locomotion to enforce a predefined step length.
+	/// Measure for locomotion that enforces a constant stride length.
 	/** Example:
 	\verbatim
 	StepMeasure {
-		step_length { min = 0.1 max = 0.4 abs_penalty = 1 }
+		stride_length { min = 1.0 max = 1.4 abs_penalty = 1 }
 	}
 	\endverbatim
 	*/
@@ -25,24 +25,24 @@ namespace scone
 	public:
 		StepMeasure( const PropNode& props, Params& par, const Model& model, const Location& loc );
 
-		/// Penalty for step length [m] when out of range.
-		RangePenalty<double> step_length;
+		/// Penalty for stride length [m] when out of range.
+		RangePenalty<double> stride_length;
 
-		/// Load threshold for step detection; default = 0.2.
+		/// Load threshold for step detection; default = 0.01.
 		Real load_threshold;
 
 		/// Minimum duration [s] of a step, used for step detection; default = 0.2.
 		Real min_step_duration;
 
-		/// Number of initial steps of which the velocity is disregarded in the final measure; default = 2.
-		int initiation_steps;
+		/// Number of initial gait cycles of which the calculation is
+		/// disregarded in the final measure; default = 1.
+		int initiation_cycles;
 
 		virtual bool UpdateMeasure( const Model& model, double timestamp ) override;
 		virtual double ComputeResult( const Model& model ) override;
 
 	protected:
 		virtual String GetClassSignature() const override;
-		virtual void StoreData( Storage< Real >::Frame& frame, const StoreDataFlags& flags ) const override;
 
 	private:
 		Storage<Real> stored_data_;
