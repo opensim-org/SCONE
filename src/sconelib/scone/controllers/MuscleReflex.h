f476953ac2eb3b8c10b2cc8929bf602ce18c3fd7
Dimitar Stanev <jimstanev@gmail.com>
2020-03-23 12:25:31 +0100

    change allow negative behavior for MuscleReflex

--- src/sconelib/scone/controllers/MuscleReflex.h
+++ src/sconelib/scone/controllers/MuscleReflex.h
@@ -15,9 +15,9 @@
 namespace scone
 {
 	/// Reflex based on muscle length, muscle velocity, muscle force, or muscle spindle sensor.
-	/// 
+	///
 	/// Must be part of ReflexController. Output excitation corresponds to
-	/// ''U = _C0_ + [_KF(F - F0)_]<sub>+</sub> + [_KL(L - L0)_]<sub>+</sub> + [_KV(V - V0)_]<sub>+</sub> + [_KS(S - S0)_]<sub>+</sub>'',
+	/// ''U = _C0_ + _KF[(F - F0)_]<sub>+</sub> + _KL[(L - L0)_]<sub>+</sub> + _KV[(V - V0)_]<sub>+</sub> + _KS[(S - S0)_]<sub>+</sub>'',
 	/// where []<sub>+</sub> indicates a result is always >= 0, unless the corresponding allow_neg_* is set to 1.
 	class MuscleReflex : public Reflex
 	{
@@ -82,8 +82,9 @@ namespace scone
 	private:
 		Real GetValue( SensorDelayAdapter* s, Real gain, Real ofs, bool allow_neg ) {
 			if ( s ) {
-				Real value = gain * ( s->GetValue( delay ) - ofs );
-				return ( !allow_neg && value < 0.0 ) ? 0.0 : value;
+				Real sensoryFeedback = ( s->GetValue( delay ) - ofs );
+				sensoryFeedback = ( !allow_neg && sensoryFeedback < 0.0 ) ? 0.0 : sensoryFeedback;
+				return gain * sensoryFeedback;
 			}
 			else return 0.0;
 		}
