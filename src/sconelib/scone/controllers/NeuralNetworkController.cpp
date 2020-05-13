e1483fb8c9d00e1acf9475cc94573d414965427c
Thomas Geijtenbeek <tgeijten@gmail.com>
2020-05-13 16:14:50 +0200

    U!: NeuralNetworkController now supports components

--- src/sconelib/scone/controllers/NeuralNetworkController.cpp
+++ src/sconelib/scone/controllers/NeuralNetworkController.cpp
@@ -1,6 +1,7 @@
 #include "NeuralNetworkController.h"
 
 #include "xo/string/string_tools.h"
+#include "scone/core/Log.h"
 #include "scone/model/Model.h"
 #include "scone/model/Muscle.h"
 #include "scone/model/Dof.h"
@@ -10,6 +11,7 @@
 #include "xo/utility/irange.h"
 #include "scone/core/HasName.h"
 #include "scone/model/MuscleId.h"
+#include "xo/utility/hash.h"
 
 namespace scone::NN
 {
@@ -17,54 +19,28 @@ namespace scone::NN
 		Controller( pn, par, model, area ),
 		INIT_MEMBER_REQUIRED( pn, neural_delays_ )
 	{
-		// create sensor neurons
-		neurons_.emplace_back();
-		for ( const auto& mus : model.GetMuscles() )
+		for ( const auto& [key, child_pn] : pn )
 		{
-			auto name = GetNameNoSide( mus->GetName() );
-			AddSensor( &model.AcquireDelayedSensor<MuscleLengthSensor>( *mus ), neural_delays_[ name ], -1 );
-			AddSensor( &model.AcquireDelayedSensor<MuscleForceSensor>( *mus ), neural_delays_[ name ], 0 );
-		}
-		auto name = "pelvis_tilt";
-		auto kv = par.get( "pelvis_tilt.DPV.DV", 0.1, 0.01, -10, 10 );
-		AddSensor( &model.AcquireDelayedSensor<DofPosVelSensor>( *FindByName( model.GetDofs(), name ), kv ), neural_delays_[ name ], 0 );
-
-		// create inter neurons
-
-		// create motor neurons
-		neurons_.emplace_back();
-		for ( const auto& mus : model.GetMuscles() )
-		{
-			auto& neuron = AddActuator( mus.get() );
-			const auto par_name = GetNameNoSide( mus->GetName() );
-			neuron.offset_ = par.get( par_name + ".C0", 0.0, 0.01, -10.0, 10.0 );
-		}
-
-		// create direct links
-		links_.emplace_back();
-		for ( index_t sidx : xo::irange( sensor_links_.size() ) )
-		{
-			for ( auto aidx : xo::irange( motor_links_.size() ) )
-			{
-				const auto& sname = xo::split_str_at_last( sensor_links_[ sidx ].sensor_->GetName(), "." );
-				const auto& aname = motor_links_[ aidx ].actuator_->GetName();
-				auto smi = MuscleId( sname.first );
-				auto ami = MuscleId( aname );
-				if ( smi.side_ == NoSide || smi.side_ == ami.side_ )
-				{
-					// do some name stuff
-					auto parname = ami.base_;
-					auto sparname = smi.base_;
-					if ( parname != sparname )
-						parname += '.' + sparname;
-					parname += '.' + sname.second;
-
-					// create the link
-					double weight = par.get( parname, 0.0, 0.01, -10.0, 10.0 );
-					links_.back().push_back( Link{ sidx, aidx, weight } );
-				}
+			try {
+				CreateComponent( key, child_pn, par, model );
+			}
+			catch ( std::exception& e ) {
+				SCONE_ERROR( "Error in " + key + ": " + e.what() );
 			}
 		}
+	}
+
+	NeuronLayer& NeuralNetworkController::AddNeuronLayer( index_t layer )
+	{
+		neurons_.resize( std::max( neurons_.size(), layer + 1 ) );
+		return neurons_[ layer ];
+	}
+
+	LinkLayer& NeuralNetworkController::AddLinkLayer( index_t input_layer, index_t output_layer )
+	{
+		SCONE_ASSERT( output_layer > 0 );
+		links_.resize( std::max( links_.size(), output_layer ) );
+		return links_[ output_layer - 1 ].emplace_back( input_layer );
 	}
 
 	Neuron& NeuralNetworkController::AddSensor( SensorDelayAdapter* sensor, TimeInSeconds delay, double offset )
@@ -93,8 +69,9 @@ namespace scone::NN
 		// update links and inter neurons
 		for ( index_t idx = 0; idx < links_.size(); ++idx )
 		{
-			for ( const auto& link : links_[ idx ] )
-				neurons_[ idx + 1 ][ link.trg_idx_ ].input_ += link.weight_ * neurons_[ idx ][ link.src_idx_ ].output_;
+			for ( const auto& link_layer : links_[ idx ] )
+				for ( const auto& link : link_layer.links_ )
+					neurons_[ idx + 1 ][ link.trg_idx_ ].input_ += link.weight_ * neurons_[ link_layer.input_layer_ ][ link.src_idx_ ].output_;
 			for ( auto& neuron : neurons_[ idx + 1 ] )
 				update_output( neuron );
 		}
@@ -108,17 +85,141 @@ namespace scone::NN
 
 	void NeuralNetworkController::StoreData( Storage<Real>::Frame& frame, const StoreDataFlags& flags ) const
 	{
-		for ( const auto& link : links_.back() )
+		for ( const auto& sn : sensor_links_ )
+			frame[ "SN." + sn.sensor_->GetName() ] = neurons_.front()[ sn.neuron_idx_ ].output_;
+
+		for ( auto& mn : motor_links_ )
+			frame[ "MN." + mn.actuator_->GetName() ] = neurons_.back()[ mn.neuron_idx_ ].output_;
+	}
+
+	PropNode NeuralNetworkController::GetInfo() const
+	{
+		PropNode pn;
+		for ( const auto& sn : sensor_links_ )
+			pn[ sn.sensor_->GetName() ] = neurons_.front()[ sn.neuron_idx_ ].offset_;
+
+		for ( const auto& il : links_.front().front().links_ )
 		{
-			auto& sl = sensor_links_[ link.src_idx_ ];
-			auto& al = motor_links_[ link.trg_idx_ ];
-			String str = "MN." + al.actuator_->GetName() + '.' + sl.sensor_->GetName();
-			frame[ str ] = link.weight_ * neurons_.front()[ link.src_idx_ ].output_;
+			PropNode lpn;
+			lpn[ "src_idx" ] = il.src_idx_;
+			lpn[ "trg_idx" ] = il.trg_idx_;
+			lpn[ "weight" ] = il.weight_;
+			pn.add_child( "link", lpn );
 		}
+
+		for ( const auto& mn : motor_links_ )
+			pn[ mn.actuator_->GetName() ] = neurons_.back()[ mn.neuron_idx_ ].offset_;
+
+		return pn;
 	}
 
 	String NeuralNetworkController::GetClassSignature() const
 	{
 		return xo::stringf( "NN%d", neurons_.size() );
+	}
+
+	String NeuralNetworkController::GetParName( const String& source, const String& target, const String& type )
+	{
+		auto tns = GetNameNoSide( target );
+		auto sns = GetNameNoSide( source );
+		if ( tns == sns )
+			return tns + '.' + type;
+		else return tns + '.' + sns + '.' + type;
+	}
+
+	void NeuralNetworkController::CreateComponent( const String& key, const PropNode& pn, Params& par, Model& model )
+	{
+		switch ( xo::hash( key ) )
+		{
+		case "MuscleSensors"_hash:
+		{
+			AddNeuronLayer( 0 );
+			bool length = pn.get<bool>( "length", true );
+			bool force = pn.get<bool>( "force", true );
+			auto include = pn.get<xo::pattern_matcher>( "include", "" );
+			for ( const auto& mus : model.GetMuscles() )
+			{
+				if ( include.empty() || include( mus->GetName() ) )
+				{
+					auto delay = neural_delays_[ GetNameNoSide( mus->GetName() ) ];
+					if ( length ) AddSensor( &model.AcquireDelayedSensor<MuscleLengthSensor>( *mus ), delay, -1 );
+					if ( force ) AddSensor( &model.AcquireDelayedSensor<MuscleForceSensor>( *mus ), delay, 0 );
+				}
+			}
+			break;
+		}
+		case "BodyAngularVelocitySensor"_hash:
+		{
+			AddSensor( &model.AcquireDelayedSensor<BodyAngularVelocitySensor>(
+				*FindByName( model.GetBodies(), pn.get<String>( "body" ) ),
+				pn.get<Vec3>( "dir" ), pn.get<String>( "id" ) ), pn.get<double>( "delay" ), 0 );
+			break;
+		}
+		case "BodyOrientationSensor"_hash:
+		{
+			AddSensor( &model.AcquireDelayedSensor<BodyOrientationSensor>(
+				*FindByName( model.GetBodies(), pn.get<String>( "body" ) ),
+				pn.get<Vec3>( "dir" ), pn.get<String>( "id" ) ), pn.get<double>( "delay" ), 0 );
+			break;
+		}
+		case "DofPosVelSensor"_hash:
+		{
+			auto dof = pn.get<String>( "dof" );
+			auto kv = par.try_get( dof + ".DPV.DV", pn, "velocity_gain", 0.1 );
+			AddSensor( &model.AcquireDelayedSensor<DofPosVelSensor>( *FindByName( model.GetDofs(), dof ), kv ), neural_delays_[ dof ], 0 );
+			break;
+		}
+		case "InterNeurons"_hash:
+		{
+			break;
+		}
+		case "MotorNeurons"_hash:
+		{
+			AddNeuronLayer( pn.get<index_t>( "layer" ) );
+			auto include = pn.get<xo::pattern_matcher>( "include", "" );
+			for ( const auto& mus : model.GetMuscles() )
+			{
+				if ( include.empty() || include( mus->GetName() ) )
+				{
+					auto& neuron = AddActuator( mus.get() );
+					neuron.offset_ = par.get( GetNameNoSide( mus->GetName() ) + ".C0", pn.get_child( "offset" ) );
+				}
+			}
+			break;
+		}
+		case "Link"_hash:
+		{
+			auto input_layer_idx = pn.get<index_t>( "input_layer" );
+			auto output_layer_idx = pn.get<index_t>( "output_layer" );
+			auto& link_layer = AddLinkLayer( input_layer_idx, output_layer_idx );
+			bool source_is_sensor = input_layer_idx == 0;
+			bool target_is_motor = output_layer_idx == neurons_.size() - 1;
+			for ( index_t source_neuron_idx : xo::irange( neurons_[ input_layer_idx ].size() ) )
+			{
+				for ( auto target_neuron_idx : xo::irange( neurons_[ output_layer_idx ].size() ) )
+				{
+					auto source_name_full = source_is_sensor ? sensor_links_[ source_neuron_idx ].sensor_->GetName() :
+						xo::stringf( "I%d.%d.value", input_layer_idx, source_neuron_idx );
+					auto target_name = target_is_motor ? motor_links_[ target_neuron_idx ].actuator_->GetName() :
+						xo::stringf( "I%d.%d.value", output_layer_idx, source_neuron_idx );
+					auto [source_name, source_type] = xo::split_str_at_last( source_name_full, "." );
+
+					if ( source_is_sensor && target_is_motor )
+					{
+						auto smi = MuscleId( source_name );
+						auto ami = MuscleId( target_name );
+						if ( smi.side_ == NoSide || smi.side_ == ami.side_ )
+						{
+							auto parname = GetParName( source_name, target_name, source_type );
+							double weight = par.get( parname, pn.get_child( "weight" ) );
+							link_layer.links_.push_back( Link{ source_neuron_idx, target_neuron_idx, weight } );
+						}
+					}
+				}
+			}
+		}
+		default:
+			break;
+		}
 	}
 }
--- src/sconelib/scone/controllers/NeuralNetworkController.h
+++ src/sconelib/scone/controllers/NeuralNetworkController.h
@@ -13,11 +13,18 @@ namespace scone
 			double offset_ = 0;
 			double output_ = 0;
 		};
+		using NeuronLayer = std::vector<Neuron>;
 
 		struct Link {
 			index_t src_idx_;
 			index_t trg_idx_;
 			double weight_ = 0;
+		};
+		struct LinkLayer
+		{
+			LinkLayer( index_t input_layer ) : input_layer_( input_layer ) {}
+			index_t input_layer_;
+			std::vector<Link> links_;
 		};
 
 		struct SensorNeuronLink {
@@ -41,20 +48,26 @@ namespace scone
 			NeuralNetworkController( const PropNode& props, Params& par, Model& model, const Location& target_area );
 			virtual ~NeuralNetworkController() {}
 
-			Neuron& AddSensor( SensorDelayAdapter* sensor, TimeInSeconds delay, double offset );
-			Neuron& AddActuator( Actuator* actuator );
-
 			void StoreData( Storage<Real>::Frame& frame, const StoreDataFlags& flags ) const override;
+			PropNode GetInfo() const;
 
 		protected:
 			bool ComputeControls( Model& model, double timestamp ) override;
 			String GetClassSignature() const override;
 
 		private:
+
+			NeuronLayer& AddNeuronLayer( index_t layer );
+			LinkLayer& AddLinkLayer( index_t input_layer, index_t output_layer );
+			Neuron& AddSensor( SensorDelayAdapter* sensor, TimeInSeconds delay, double offset );
+			Neuron& AddActuator( Actuator* actuator );
+			String GetParName( const String& target, const String& source, const String& type );
+			void CreateComponent( const String& key, const PropNode& pn, Params& par, Model& model );
+
 			const xo::flat_map< String, TimeInSeconds > neural_delays_;
 			std::vector<SensorNeuronLink> sensor_links_;
-			std::vector< std::vector<Neuron> > neurons_;
-			std::vector< std::vector<Link> > links_;
+			std::vector< NeuronLayer > neurons_;
+			std::vector< std::vector< LinkLayer > > links_;
 			std::vector<MotorNeuronLink> motor_links_;
 		};
 	}
