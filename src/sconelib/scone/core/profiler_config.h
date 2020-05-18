9e28ce3329df9996e236b5a8b34ad460f74eda15
Thomas Geijtenbeek <tgeijten@gmail.com>
2020-05-18 16:42:34 +0200

    F: profiler_config SCONE_PROFILE_* macros when profiler is disabled

--- src/sconelib/scone/core/profiler_config.h
+++ src/sconelib/scone/core/profiler_config.h
@@ -15,11 +15,7 @@
 #	include "xo/system/profiler.h"
 #	define SCONE_PROFILE_FUNCTION( profiler ) xo::scope_profiler scoped_profile_var( __FUNCTION__, profiler )
 #	define SCONE_PROFILE_SCOPE( profiler, scope_name_arg ) xo::scope_profiler scoped_profile_var( scope_name_arg, profiler )
-// #	define SCONE_PROFILE_START xo::profiler::instance().start()
-// #	define SCONE_PROFILE_REPORT xo::profiler::instance().log_results()
 #else 
-#	define SCONE_PROFILE_FUNCTION void()
-#	define SCONE_PROFILE_SCOPE( scope_name_arg )
-#	define SCONE_PROFILE_START void()
-#	define SCONE_PROFILE_REPORT PropNode()
+#	define SCONE_PROFILE_FUNCTION( profiler ) void()
+#	define SCONE_PROFILE_SCOPE( profiler, scope_name_arg ) void()
 #endif
