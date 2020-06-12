3378655e9e261711ad2026a8b1b61f9b9944b545
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2020-06-13 00:01:20 +0200

    U*: BodyMeasure now uses ''direction'' and has option for magnitude

--- src/sconelib/scone/measures/BodyMeasure.cpp
+++ src/sconelib/scone/measures/BodyMeasure.cpp
@@ -19,14 +19,14 @@ namespace scone
 	range_count( 0 )
 	{
 		INIT_PROP( props, offset, Vec3::zero() );
-		INIT_PROP( props, axes_weights, Vec3::zero() );
+		INIT_PROP( props, direction, Vec3::zero() );
 		INIT_PROP( props, relative_to_model_com, false );
+		INIT_PROP( props, magnitude, direction.is_null() );
 		INIT_PROP( props, position, RangePenalty<Real>() );
 		INIT_PROP( props, velocity, RangePenalty<Real>() );
 		INIT_PROP( props, acceleration, RangePenalty<Real>() );
 
 		range_count = int( !position.IsNull() ) + int( !velocity.IsNull() ) + int( !acceleration.IsNull() );
-		name = body.GetName();
 	}
 
 	double BodyMeasure::ComputeResult( const Model& model )
@@ -36,19 +36,19 @@ namespace scone
 		{
 			penalty += position.GetResult();
 			if ( range_count > 1 )
-				GetReport().set( name + ".pos_limit", stringf( "%g", position.GetResult() ) );
+				GetReport().set( name + ".pos_penalty", stringf( "%g", position.GetResult() ) );
 		}
 		if ( !velocity.IsNull() )
 		{
 			penalty += velocity.GetResult();
 			if ( range_count > 1 )
-				GetReport().set( name + ".vel_limit", stringf( "%g", velocity.GetResult() ) );
+				GetReport().set( name + ".vel_penalty", stringf( "%g", velocity.GetResult() ) );
 		}
 		if ( !acceleration.IsNull() )
 		{
 			penalty += acceleration.GetResult();
 			if ( range_count > 1 )
-				GetReport().set( name + ".acc_limit", stringf( "%g", acceleration.GetResult() ) );
+				GetReport().set( name + ".acc_penalty", stringf( "%g", acceleration.GetResult() ) );
 		}
 
 		return  penalty;
@@ -60,21 +60,21 @@ namespace scone
 		{
 			auto pos = body.GetPosOfPointOnBody( offset );
 			if ( relative_to_model_com ) pos -= model.GetComPos();
-			position.AddSample( timestamp, dot_product( axes_weights, pos ) );
+			position.AddSample( timestamp, magnitude ? length( pos ) : dot_product( direction, pos ) );
 		}
 
 		if ( !velocity.IsNull() )
 		{
 			auto vel = body.GetLinVelOfPointOnBody( offset );
 			if ( relative_to_model_com ) vel -= model.GetComVel();
-			velocity.AddSample( timestamp, dot_product( axes_weights, vel ) );
+			velocity.AddSample( timestamp, magnitude ? length( vel ) : dot_product( direction, vel ) );
 		}
 
 		if ( !acceleration.IsNull() )
 		{
 			auto acc = body.GetLinAccOfPointOnBody( offset );
 			if ( relative_to_model_com ) acc -= model.GetComAcc();
-			acceleration.AddSample( timestamp, dot_product( axes_weights, acc ) );
+			acceleration.AddSample( timestamp, magnitude ? length( acc ) : dot_product( direction, acc ) );
 		}
 
 		return false;
@@ -88,10 +88,10 @@ namespace scone
 	void BodyMeasure::StoreData( Storage< Real >::Frame& frame, const StoreDataFlags& flags ) const
 	{
 		if ( !position.IsNull() )
-			frame[ GetName() + ".pos_limit" ] = position.GetLatest();
+			frame[ body.GetName() + ".pos_penalty" ] = position.GetLatest();
 		if ( !velocity.IsNull() )
-			frame[ GetName() + ".vel_limit" ] = velocity.GetLatest();
+			frame[ body.GetName() + ".vel_penalty" ] = velocity.GetLatest();
 		if ( !acceleration.IsNull() )
-			frame[ GetName() + ".acc_limit" ] = acceleration.GetLatest();
+			frame[ body.GetName() + ".acc_penalty" ] = acceleration.GetLatest();
 	}
 }
