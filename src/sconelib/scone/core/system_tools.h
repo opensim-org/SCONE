0fb40563bdece555d62a265179f54a1b46a0fcfb
Adam Kewley <contact@adamkewley.com>
2020-06-25 12:42:25 +0100

    Fixed typo'd header update

--- src/sconelib/scone/core/system_tools.h
+++ src/sconelib/scone/core/system_tools.h
@@ -16,8 +16,8 @@ namespace scone
 	using xo::path;
 
 	enum SconeFolder { SCONE_ROOT_FOLDER, SCONE_RESULTS_FOLDER, SCONE_MODEL_FOLDER, SCONE_SCENARIO_FOLDER, SCONE_RESOURCE_FOLDER, SCONE_GEOMETRY_FOLDER, SCONE_UI_RESOURCE_FOLDER };
-	SCONE_API const path& GetSettingsFolder();
-	SCONE_API path GetInstallFolder();
+	SCONE_API path GetSettingsFolder();
+	SCONE_API const path& GetInstallFolder();
 	SCONE_API path GetDataFolder();
 	SCONE_API path GetFolder( SconeFolder folder );
 	SCONE_API path FindFile( const path& filename );
