43c5610218ee4fa2553a22c01e24131dc0bbb49f
Adam Kewley <contact@adamkewley.com>
2020-06-25 15:25:27 +0200

    Make getinstallfolder use local static (#211)
    * Changed GetInstallFolder to use a initilized-once local static variable
    
    * Rename GetInstallFolderUncached to FindInstallFolder for clarity
    
    * Use C++17 initialize-in-if feature
    
    * Made GetInstallFolder return a path const& rather than a path value
    
    * Fixed not updating the header
    
    * Fixed typo'd header update

--- src/sconelib/scone/core/system_tools.h
+++ src/sconelib/scone/core/system_tools.h
@@ -17,7 +17,7 @@ namespace scone
 
 	enum SconeFolder { SCONE_ROOT_FOLDER, SCONE_RESULTS_FOLDER, SCONE_MODEL_FOLDER, SCONE_SCENARIO_FOLDER, SCONE_RESOURCE_FOLDER, SCONE_GEOMETRY_FOLDER, SCONE_UI_RESOURCE_FOLDER };
 	SCONE_API path GetSettingsFolder();
-	SCONE_API path GetInstallFolder();
+	SCONE_API const path& GetInstallFolder();
 	SCONE_API path GetDataFolder();
 	SCONE_API path GetFolder( SconeFolder folder );
 	SCONE_API path FindFile( const path& filename );
