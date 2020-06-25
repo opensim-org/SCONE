b25d1cee21bf63f8a4bbda8fa7eed1657fd7d177
Adam Kewley <contact@adamkewley.com>
2020-06-25 12:30:28 +0100

    Fixed not updating the header

--- src/sconelib/scone/core/system_tools.h
+++ src/sconelib/scone/core/system_tools.h
@@ -16,7 +16,7 @@ namespace scone
 	using xo::path;
 
 	enum SconeFolder { SCONE_ROOT_FOLDER, SCONE_RESULTS_FOLDER, SCONE_MODEL_FOLDER, SCONE_SCENARIO_FOLDER, SCONE_RESOURCE_FOLDER, SCONE_GEOMETRY_FOLDER, SCONE_UI_RESOURCE_FOLDER };
-	SCONE_API path GetSettingsFolder();
+	SCONE_API const path& GetSettingsFolder();
 	SCONE_API path GetInstallFolder();
 	SCONE_API path GetDataFolder();
 	SCONE_API path GetFolder( SconeFolder folder );
