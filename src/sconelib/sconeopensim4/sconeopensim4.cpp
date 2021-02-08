9e7b8a7f9d3237df5668d53d2bc813b917cb917e
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2021-02-08 12:38:31 +0100

    U: RegisterSconeOpenSim4 now only registers "ModelOpenSim4"

--- src/sconelib/sconeopensim4/sconeopensim4.cpp
+++ src/sconelib/sconeopensim4/sconeopensim4.cpp
@@ -1,4 +1,4 @@
-#include "sconeopensim3.h"
+#include "sconeopensim4.h"
 
 #include "scone/core/Factories.h"
 #include "ModelOpenSim4.h"
@@ -7,9 +7,6 @@ namespace scone
 {
 	SCONE_OPENSIM_4_API void RegisterSconeOpenSim4()
 	{
-		GetModelFactory().register_type< ModelOpenSim4 >( "Simbody" ); // backwards compatibility
-		GetModelFactory().register_type< ModelOpenSim4 >( "OpenSim4Model" );
-		GetModelFactory().register_type< ModelOpenSim4 >( "OpenSimModel" );
 		GetModelFactory().register_type< ModelOpenSim4 >( "ModelOpenSim4" );
 	}
 }
