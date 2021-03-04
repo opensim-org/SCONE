58db6a514d4ef54ddaeae5958e6f25dfa07a586c
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2021-03-04 17:13:49 +0100

    V: 2.0.0 ALPHA 2

--- src/sconelib/scone/core/version.cpp
+++ src/sconelib/scone/core/version.cpp
@@ -16,7 +16,7 @@
 constexpr int SCONE_VERSION_MAJOR = 2;
 constexpr int SCONE_VERSION_MINOR = 0;
 constexpr int SCONE_VERSION_PATCH = 0;
-constexpr const char* SCONE_VERSION_POSTFIX = "ALPHA 1";
+constexpr const char* SCONE_VERSION_POSTFIX = "ALPHA 2";
 
 namespace scone
 {
